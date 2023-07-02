44a29f9cb1b02eb42c33098169cbd1fb3f550236
wladimirrojas <rajimiru@gmail.com>
2023-07-02 02:55:42 -0400

    added task list for users

--- src/main/java/com/foodapp/foodtruck/controller/UserController.java
+++ src/main/java/com/foodapp/foodtruck/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.foodapp.foodtruck.controller;
 
 import com.foodapp.foodtruck.dto.UserDto;
+import com.foodapp.foodtruck.model.Task;
+import com.foodapp.foodtruck.services.TaskService;
 import com.foodapp.foodtruck.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -18,12 +22,14 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final TaskService taskService;
 
     //private final UserMapper mapper = new UserMapper();
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, TaskService taskService) {
         this.userService = userService;
+        this.taskService = taskService;
     }
 
     @GetMapping("/list")
@@ -40,4 +46,21 @@ public class UserController {
     public ResponseEntity deleteUser(@RequestBody() UserDto user) {
         return ResponseEntity.ok(userService.deleteBy(user));
     }
+
+    @PostMapping("/task/save")
+    public ResponseEntity<Boolean> saveTask(@RequestBody() Task task) {
+        task.setCreationDate(LocalDateTime.now());
+        return ResponseEntity.ok(taskService.saveTask(task));
+    }
+
+    @GetMapping("/task/list/{creatorRut}")
+    public ResponseEntity<List<Task>> getTaskByCreatorRut(@PathVariable("creatorRut") String creatorRut) {
+        return ResponseEntity.ok(taskService.getTaskByCreatorRut(creatorRut));
+    }
+
+    @DeleteMapping("/task/delete/{id}")
+    public ResponseEntity<Boolean> deleteTaskById(@PathVariable("id") String id) {
+        return ResponseEntity.ok(taskService.deleteTaskById(id));
+    }
+
 }
